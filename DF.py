9e86c423a660620547f9f4b5d47ea0ba2d08856a
opit <opit@technarium.lt>
2019-11-25 22:16:56 +0200

    state machine test code, abandoning

--- DF.py
+++ DF.py
@@ -15,13 +15,12 @@ prod = sys.argv[2]
 scene = sys.argv[3]
 #Attention: frames are numbered starting with 1, as in Dragonframe.
 
-TCP_IP = config.properties['IP']
+TCP_IP = '0.0.0.0' #config.properties['IP']
 TCP_PORT = int(config.properties['TCP_PORT'])
 PROJECT_PATH = config.properties['PROJECT_PATH']
 PROJECT_PREFIX = str(prod)
 fileName = PROJECT_PATH+prod+"_"+scene+".csv"
-print(fileName)
-BUFFER_SIZE = 4
+BUFFER_SIZE = 16
 PREFIX = 8 
 HEADER1 = 11
 HEADER2 = 2
@@ -32,8 +31,7 @@ vel = 1.
 dur = 999
 move_delay = 0.6 	
 DFControl = True
-manualControl = False
-demoLoop = False
+
 
 def setPos():
 	#return [10, 10, 15, 30, 60, 50]
@@ -61,24 +59,8 @@ def getFileLength(fileName):
 	input_file.close()
 
 
-def setZeroPos():
-	return [0, 0, 0, 0, 0, 0]
-
-def makeDemoLoop(times):
- for i in range(0, times):
- 	joint_pos = setZeroPos()
- 	sendMsg(joint_pos, dummy_joint, PREFIX, HEADER1, HEADER2, HEADER3, SEQUENCE, vel, dur)
- 	time.sleep(1)
- 	joint_pos = setPos()
- 	sendMsg(joint_pos, dummy_joint, PREFIX, HEADER1, HEADER2, HEADER3, SEQUENCE, vel, dur)
- 	time.sleep(1)
-			
-
-
-
 #https://pymotw.com/2/socket/binary.html
-coordLength = getFileLength(fileName)
-#coordLength = 2 #for testing safety
+
 
 
 class GracefulKiller:
@@ -86,7 +68,7 @@ class GracefulKiller:
   def __init__(self):
     # register the signals to be caught
     signal.signal(signal.SIGHUP, self.exit_gracefully)
-    signal.signal(signal.SIGINT, self.exit_gracefully)
+    #signal.signal(signal.SIGINT, self.exit_gracefully)
     signal.signal(signal.SIGQUIT, self.exit_gracefully)
     signal.signal(signal.SIGILL, self.exit_gracefully)
     signal.signal(signal.SIGTRAP, self.exit_gracefully)
@@ -104,147 +86,103 @@ class GracefulKiller:
 
   def exit_gracefully(self,signum, frame):
     #self.kill_now = True
-    with open('/Users/primavera/Desktop/Staubli/log.txt','ab') as f:
-    	f.write("Signum: "+str(signum)+"\n")  #saving the set frame
+    with open(PROJECT_PATH+'log.txt','ab') as f:
+    	f.write("Sig: "+str(signum)+"\n")  #saving the set frame
     	f.close()	
 
+
+#time.sleep(3) #time to run to the robot 
+a = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+a.connect((TCP_IP, TCP_PORT))
+			
+
+			
+
+def log(msg):
+	with open(PROJECT_PATH+'log.txt','ab') as f:				
+		f.write(str(msg)+"\n") 
+		f.close()
+
+def getCurrPos():
+	f = open(PROJECT_PATH+"current_pos.csv", "r")
+	start = int(f.read())
+	f.close()
+	return start
+
+def setCurrPos(frame):
+	with open(PROJECT_PATH+'current_pos.csv','wb') as f:
+		f.write(str(frame))  #saving the set frame
+		f.close()
+
+
 def sendMsg(joint_pos, dummy_joint, PREFIX, HEADER1, HEADER2, HEADER3, SEQUENCE, vel, dur):
-	msg = (PREFIX, HEADER1, HEADER2, HEADER3, SEQUENCE, joint_pos[0], joint_pos[1], joint_pos[2], joint_pos[3], joint_pos[4], joint_pos[5], dummy_joint, dummy_joint, dummy_joint, dummy_joint, vel, dur)
-	packer = struct.Struct('I I I I I f f f f f f f f f f f f')
-	packed_data = packer.pack(*msg)
-	print('sending "%s"' % binascii.hexlify(packed_data), msg)
-	return packed_data#print(msg)
+	msg = [PREFIX, HEADER1, HEADER2, HEADER3, SEQUENCE, joint_pos[0], joint_pos[1], joint_pos[2], joint_pos[3], joint_pos[4], joint_pos[5], dummy_joint, dummy_joint, dummy_joint, dummy_joint, vel, dur]
+	#packer = struct.Struct('I I I I I f f f f f f f f f f f f')
+	#packed_data = packer.pack(*msg)
+	#print('sending "%s"' % binascii.hexlify(packed_data), msg)
+	out = ''.join(map(str, msg))
+	print(out, "msg")
+	return out #packed_data
+
+if __name__ == "__main__":
+		state = 'init'
+		killer = GracefulKiller()
+		coordLength = getFileLength(fileName)
+		end = frame
+		start = getCurrPos()
+		curr_frame = start		
+		if (abs(start - end) >= coordLength+1):
+			log("Wrong trajectory or file, trajectory length: "+ str(abs(start - end)) + " File length: "+str(coordLength))
+			state = 'fail'
+		else:
+			state = 'initMotion'
+		log("DF cmd received")	
+		if state == 'initMotion':
+			time.sleep(30)				
+			while (curr_frame<>end):
+				print("current frame: ", curr_frame)
+				if (start < end):
+						if (curr_frame < end):
+							curr_frame = curr_frame + 1
+				elif (start > end):
+						if (curr_frame > end):
+							curr_frame = curr_frame - 1
+				state = "getDFPos"		
+				if state == 'getDFPos':
+					if (curr_frame > 0 & curr_frame < coordLength+1):
+						joint_pos = getDFPos(curr_frame) #TODO: handle IOError
+						print("joints: ",joint_pos)
+
+
+
+						state = 'sendCommand'
+					else:
+						log("Frame outside bounds")
+						state == 'fail'				
+				if state == 'sendCommand':
+					try:
+						a.sendall("goto "+str(curr_frame))
+						state = 'recvCommand'
+					except IOError as e:
+						state='fail'
+						log(str(e))
+				if state == 'recvCommand':
+					try:
+						data = a.recv(BUFFER_SIZE) #TODO: handle robot error message
+						print("Robot has arrived to a point")
+						setCurrPos(str(curr_frame))
+						state = 'initMotion'
+						#a.close()						
+					except IOError as e:
+						state='fail'
+						log(str(e))
+			if state == 'fail':
+				log("failed state")			
+
 	
 
-f = open(PROJECT_PATH+"current_pos.csv", "r")
-start = int(f.read())
-f.close()
-#time.sleep(3) #time to run to the robot 
 
-
-def goto(frame):
-	print("goto ", frame)
-	if (frame > 0 & frame < coordLength+1):
-		joint_pos = getDFPos(frame)
-		print("joints: ",joint_pos)
-		
-		try:
-			a = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			a.connect((TCP_IP, TCP_PORT))
-			packed_data = sendMsg(joint_pos, dummy_joint, PREFIX, HEADER1, HEADER2, HEADER3, SEQUENCE, vel, dur)	
-			a.sendall(packed_data)			
-			ret = 0
-			msg = [0]*17
-			for i in range(0, 17):
-			 	data = a.recv(BUFFER_SIZE)
-			 	msg[i] = from_binary(data)[0]
-			print("msg: ", msg) 	
-		 	ret = msg[3]
-		 	time.sleep(move_delay)
-
-			#a.shutdown(socket.SHUT_WR)
-			a.close()
-			if (ret == 1):
-			 	print("Robot has arrived to a point")
-			 	
-				with open(PROJECT_PATH+'current_pos.csv','wb') as f:
-					f.write(str(frame))  #saving the set frame, TODO: move after robot ack
-					f.close()
-				with open(PROJECT_PATH+'log.txt','ab') as f:
-					f.write("Step to: "+str(frame)+"\n")  #saving the set frame
-					f.close()
-			
-			else:
-				print("Robot is stuck, ret = ", ret)
-				with open(PROJECT_PATH+'log.txt','ab') as f:
-					f.write("Robot is stuck at: "+str(frame)+"\n")  #saving the set frame
-					f.close()
-	 	
-			return ret
-		except IOError as e:
-			if e.errno == errno.EPIPE:
-				with open(PROJECT_PATH+'log.txt','ab') as f:				
-					f.write(e+" EPIPE\n") 
-					f.close()
-				time.sleep(1)			
-				a = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-				a.connect((TCP_IP, TCP_PORT))
-				packed_data = sendMsg(joint_pos, dummy_joint, PREFIX, HEADER1, HEADER2, HEADER3, SEQUENCE, vel, dur)	
-				a.sendall(packed_data)			
-				ret = 0
-				msg = [0]*17
-				for i in range(0, 17):
-				 	data = a.recv(BUFFER_SIZE)
-				 	msg[i] = from_binary(data)[0]
-				print("msg: ", msg) 	
-			 	ret = msg[3]
-			 	time.sleep(move_delay)
-
-				#a.shutdown(socket.SHUT_RDWR)
-				a.close()
-				if (ret == 1):
-				 	print("Robot has arrived to a point")
-				 	
-					with open(PROJECT_PATH+'current_pos.csv','wb') as f:
-						f.write(str(frame))  #saving the set frame, TODO: move after robot ack
-						f.close()
-					with open(PROJECT_PATH+'log.txt','ab') as f:
-						f.write("Step to: "+str(frame)+"\n")  #saving the set frame
-						f.close()
-				
-				else:
-					print("Robot is stuck, ret = ", ret)
-					with open(PROJECT_PATH+'log.txt','ab') as f:
-						f.write("Robot is stuck at: "+str(frame)+"\n")  #saving the set frame
-						f.close()
-		 	
-				return ret	
-
-if DFControl:
-		killer = GracefulKiller()
-		#while not killer.kill_now:
-		end = frame
-		curr_frame = start
-		print(end, curr_frame)
-		
-		if (abs(start - end) >= coordLength+1):
-			print("Wrong trajectory or file, trajectory length: ", abs(start - end))
-		else:
-			if (start < end):
-				while (curr_frame < end):
-					curr_frame = curr_frame + 1
-					ret = goto(curr_frame)
-					print(curr_frame)
-					time.sleep(move_delay)
-					if (ret!=1):
-						break
-			elif (start > end):
-				while (curr_frame > end):
-					curr_frame = curr_frame - 1
-					ret = goto(curr_frame)	
-					print(curr_frame)
-					time.sleep(move_delay)				
-					if (ret!=1):
-						break
-			elif (start == end):
-				print("Same point")
-				ret = goto(curr_frame)
-				time.sleep(move_delay)	
-		
-
-		
-elif manualControl:
-	#joint_pos = getDFPos(frame)
-	#joint_pos = setPos()
-	joint_pos = setZeroPos()
-	sendMsg(joint_pos, dummy_joint, PREFIX, HEADER1, HEADER2, HEADER3, SEQUENCE, vel, dur)
-
-elif demoLoop:
-	makeDemoLoop(frame)
-else:
-	print("Select motion control method!")	
-#time.sleep(0.5)
-
+	
 
 
 
